336de6fa74a56bb6dde0d8146fe72a70bc051fb9
Chris Thierer <cthierer1@gmail.com>
2018-05-12 00:21:14 -0400

    config: update production api url

--- src/config/production.js
+++ src/config/production.js
@@ -2,7 +2,7 @@
 const config = {
   baseUri: {
     galleries: 'https://cdn.stage5clingan.com/galleries',
-    coral: 'https://api.stage5clingan.com',
+    coral: 'https://6271ihgks7.execute-api.us-east-1.amazonaws.com/LATEST',
   },
   coralClient: 'https://cdn.stage5clingan.com/scripts/1/0/0/coral-client.js',
 }
